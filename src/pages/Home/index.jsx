6192dce1e3b9fb9fd968dace9597fa7eefe471a9
islomjon6975 <islomjon0919@mail.ru>
2022-04-08 15:04:03 +0500

    v/signup/2 has been done

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Opener from "../../component/Opener";
+import Signup from "../../component/Signup";
 
 export const Home = () => {
   return (
     <div>
+      <Signup />
       <Opener />
     </div>
   );

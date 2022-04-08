a92275fac5e142f66b9c89027bdd072dc628918a
islomjon6975 <islomjon0919@mail.ru>
2022-04-08 19:19:05 +0500

    footer has been done

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "../../component/Footer";
 import Opener from "../../component/Opener";
 
 export const Home = () => {
   return (
     <div>
       <Opener />
+      <Footer />
     </div>
   );
 };

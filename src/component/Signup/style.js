a082c9758ebedb2db18929ecbf6694ea4a0a8e36
islomjon6975 <islomjon0919@mail.ru>
2022-04-08 05:11:11 +0500

    signup has been done

---
+++ src/component/Signup/style.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    width: 90%;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #4C6FFF;
+    margin: auto;
+`
+
+Container.Title = styled.div`
+    font-style: normal;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 136.02%;
+    color: #FFFFFF;
+`
+
+Container.Button = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 160px;
+    height: 50px;
+    border: 1px solid #fff;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 17.3295px;
+    line-height: 25px;
+    text-transform: uppercase;
+    color: #FFFFFF;
+    margin-left: 25px;
+    border-radius: 2px;
+`
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "../../component/Navbar";
+import { Signup } from "../../component/Signup/Signup";
 
 export const Home = () => {
   return (
     <div>
+      <Signup />
       <Navbar />
     </div>
   );

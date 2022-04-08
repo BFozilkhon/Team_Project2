eaee6dfcc276e606541d772f67a2f7d5a8b800d0
q0abd <qiyomovabdulloh3@gmail.com>
2022-04-08 16:32:37 +0500

    about has done

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import About from "../../component/About";
 import Opener from "../../component/Opener";
 import Signup from "../../component/Signup";
 
@@ -7,6 +8,7 @@ export const Home = () => {
     <div>
       <Signup />
       <Opener />
+      <About />
     </div>
   );
 };
